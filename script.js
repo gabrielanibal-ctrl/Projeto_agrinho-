4b8310adafb9ef9cb49633755ba0a5cf851e0b36
gabrielanibal-ctrl <gabriel.anibal@escola.pr.gov.br>
2026-06-25 10:35:07 -0300

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+let tamanho = 16;
+
+function aumentarFonte(){
+    tamanho += 2;
+    document.body.style.fontSize = tamanho + "px";
+}
+
+function diminuirFonte(){
+    if(tamanho > 12){
+        tamanho -= 2;
+        document.body.style.fontSize = tamanho + "px";
+    }
+}    
